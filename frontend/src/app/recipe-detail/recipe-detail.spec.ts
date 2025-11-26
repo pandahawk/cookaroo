3ff490a13da5f6f6c8fef0b5c6b9f04dcba5318e
PandaHawk <mchlbng@gmail.com>
2025-11-26 10:46:49 +0100

    chore: cookaroo ux ui improvement

--- frontend/src/app/recipe-detail/recipe-detail.spec.ts
+++ frontend/src/app/recipe-detail/recipe-detail.spec.ts
@@ -1,16 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipeDetail } from './recipe-detail';
+import {ActivatedRoute, convertToParamMap} from '@angular/router';
 
 describe('RecipeDetail', () => {
   let component: RecipeDetail;
   let fixture: ComponentFixture<RecipeDetail>;
 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RecipeDetail]
-    })
-    .compileComponents();
+      imports: [RecipeDetail],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              // what your component uses: this.route.snapshot.paramMap.get('id')
+              paramMap: convertToParamMap({ id: 'test-id' }),
+            },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RecipeDetail);
     component = fixture.componentInstance;
